bd4122c203449cc156b3c310835ddeb9a5e0da84
Wu <15151851516@163.com>
2022-07-13 17:37:10 +0800

    优化button样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Button, {ButtonSize, ButtonType} from './components/button/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
-      <h2>hello world</h2>
-      <h3>你好世界</h3>
+      <Button btnType={ButtonType.Link} href={'http://www.baidu.com'}>
+        button
+      </Button>
+      <Button size={ButtonSize.Large} btnType={ButtonType.Primary}>
+        button
+      </Button>
+      <Button size={ButtonSize.Small} btnType={ButtonType.Danger} disabled={true}>
+        button
+      </Button>
     </div>
   );
 }
